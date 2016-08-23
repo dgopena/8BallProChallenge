25e5352f4b5569726dd9db57c711eb771a38c4f8
Diego Peña <diego.pena84@gmail.com>
2016-08-23 16:54:31 -0300

    Playable cue stick mechanic fully functional.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,32 +4,96 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 
     private Camera cam;
+
+    #region Input variables
+    private bool tapEnded = true;
+
     private Vector3 tapStartPos;
+    private Vector3 tapFinishPos;
+    #endregion
 
-    public float hitForce = 2f;
+    #region Play variables
+    public float hitMaxForce = 200f;
+    public float hitMaxDist = 6f;
+    private Vector3 dir;
 
-	// Use this for initialization
-	void Start () {
+    private GameObject cue; //stick
+    private GameObject hitLine; //hit trajectory
+    #endregion
+
+    // Use this for initialization
+    void Start () {
         cam = Camera.main;
-	}
+
+        cue = (GameObject)Instantiate(Resources.Load("Prefabs/Cue Stick"));
+        cue.SetActive(false);
+        hitLine = (GameObject)Instantiate(Resources.Load("Prefabs/HitLine"));
+        hitLine.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        CheckInputPC();
+    }
 
+    void CheckInputPC()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
         if (Input.GetMouseButtonDown(0))
         {
-            tapStartPos = Input.mousePosition;
-            Ray ray = cam.ScreenPointToRay(tapStartPos);
-            RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.transform.tag == "Ball")
+                tapStartPos = new Vector3(hit.point.x, 0.5f, hit.point.z);
+            }
+            tapEnded = false;
+        }
+        else if (!tapEnded)
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                tapFinishPos = new Vector3(hit.point.x, 0.5f, hit.point.z);
+
+                cue.SetActive(true);
+                hitLine.SetActive(true);
+
+                //movement and rotation of the movement line and cue stick
+                float distPoints = Vector3.Distance(tapFinishPos, tapStartPos);
+                dir = tapFinishPos - tapStartPos;
+                Vector3 pos = (distPoints / 2f) * dir.normalized + tapStartPos;
+                hitLine.transform.position = pos;
+                cue.transform.position = pos;
+                float sizeFactor = hitLine.transform.localScale.x;
+                hitLine.transform.localScale = new Vector3(sizeFactor, distPoints/2f , sizeFactor);
+
+                if(dir.magnitude > 0.05f)
                 {
-                    Vector3 forceDir = (hit.transform.position - hit.point).normalized;
-                    Rigidbody rBall = hit.transform.GetComponent<Rigidbody>();
-                    rBall.AddForce(hitForce * forceDir, ForceMode.Impulse);
+                    float angle = Mathf.Atan(dir.z / dir.x);
+                    angle = (180f * angle) / Mathf.PI;
+                    if(dir.x >= 0f)
+                    {
+                        angle += 180f;
+                    }
+                    hitLine.transform.rotation = Quaternion.Euler(90f, 0f, angle + 90f);
+                    cue.transform.rotation = Quaternion.Euler(90f, 0f, angle + 90f);
                 }
             }
         }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            tapEnded = true;
+            hitLine.SetActive(false);
+            //we calculate total force of hit, truncating it by the maximum distance possible
+            float hitLength = dir.magnitude;
+            if(hitLength > hitMaxDist)
+            {
+                hitLength = hitMaxDist;
+            }
+            float hitForce = (hitMaxForce * hitLength) / hitMaxDist;
+            cue.GetComponent<StickScript>().Hit(hitForce, dir.normalized);
+        }
     }
+
+
 }
---
+++ Assets/Scripts/StickScript.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickScript : MonoBehaviour {
+    //more than anything this class controls collsion of the stick and the animation for the movement
+    #region Movement variables
+    private Vector3 startPosition;
+    private int movePhase = 0;
+
+    private Vector3 hitDir;
+
+    private float backSpeed = 10f; //how fast does it go back
+    private float backFactor = 4f; //how back the cue goes
+    private float forwSpeed = 46f; //how fast does it go forward
+    private float forwFactor = 20f; //how forward does the cue goes
+
+    private float startTime;
+
+    private GameObject hitTarget;
+    private Vector3 hitPos;
+    #endregion
+
+    private float hitForce;
+
+    private Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        rend = transform.FindChild("Stick").GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(movePhase == 1) //backing up
+        {
+            float distCovered = (Time.time - startTime) * backSpeed;
+            float fracJourney = distCovered / backFactor;
+            transform.position = Vector3.Lerp(startPosition, (backFactor * hitDir) + startPosition, fracJourney);
+            if (fracJourney > 1)
+            {
+                movePhase++;
+                startPosition = transform.position;
+                startTime = Time.time;
+            }
+        }
+        else if(movePhase == 2) //hitting
+        {
+            float distCovered = (Time.time - startTime) * forwSpeed;
+            float fracJourney = distCovered / forwFactor;
+            transform.position = Vector3.Lerp(startPosition, (-forwFactor * hitDir) + startPosition, fracJourney);
+            if (fracJourney > 1)
+            {
+                startPosition = transform.position;
+                startTime = Time.time;
+                movePhase++;
+            }
+        }
+        else if(movePhase == 3) //post hitting back up
+        {
+            float distCovered = (Time.time - startTime) * (backSpeed/3f);
+            float fracJourney = distCovered / (backFactor/4f);
+            transform.position = Vector3.Lerp(startPosition, ((backFactor/5f) * hitDir) + startPosition, fracJourney);
+            if (fracJourney > 1)
+            {
+                movePhase = 0;
+                gameObject.SetActive(false);
+            }
+        }
+	}
+
+    public void Hit(float hitForce, Vector3 hitDir)
+    {
+        if (movePhase == 0)
+        {
+            startPosition = transform.position;
+            startTime = Time.time;
+            this.hitForce = hitForce;
+            this.hitDir = hitDir;
+            movePhase = 1;
+
+            TargetObj();
+        }
+    }
+
+    //we throw a ray to check which is the first object we'll hit
+    void TargetObj()
+    {
+        Ray ray = new Ray(transform.position, -hitDir);
+        RaycastHit hit;
+        //we get point of collision
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "Ball")
+            {
+                hitTarget = hit.transform.gameObject;
+                hitPos = hit.point;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (movePhase == 2) //only hit if the cue is in hitting phase
+        {
+            if (hitTarget != null)
+            {
+                Vector3 forceDir = (hitTarget.transform.position - hitPos).normalized;
+                Rigidbody rBall = hitTarget.GetComponent<Rigidbody>();
+                rBall.AddForce(hitForce * forceDir, ForceMode.Impulse);
+            }
+
+            //we back up
+            startPosition = transform.position;
+            startTime = Time.time;
+            movePhase++;
+        }
+    }
+}
